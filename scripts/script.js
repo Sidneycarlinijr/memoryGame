5f0594a67df638ad207f691f5e02120a91eed6eb
Sidney <sidney_carlini@hotmail.com>
2021-07-01 14:27:01 -0300

    Project done :tada:

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,7 @@ function startGame(){
 
 function initializeCards(cards){
     let gameBoard = document.getElementById("gameBoard");
+    gameBoard.innerHTML = '';
 
     cards.forEach(card=>{
         
@@ -56,19 +57,30 @@ function flipCard(){
     if(game.setCard(this.id)){
 
         this.classList.add("flip");
-        if(game.checkMatch()){
-            game.clearCards();
-        } else{
-            setTimeout(()=>{
-                let firstCardView = document.getElementById(game.firstCard.id);
-                let secondCardView = document.getElementById(game.secondCard.id);
-
-                firstCardView.classList.remove('flip');
-                secondCardView.classList.remove('flip');
+        if(game.secondCard){
+            if(game.checkMatch()){
                 game.clearCards();
-            }, 1000)
+                if(game.checkGameOver()){
+                    let gameOverLayer = document.getElementById("gameOver");
+                    gameOverLayer.style.display = 'flex';
+                };
+            } else{
+                setTimeout(()=>{
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+
+                    firstCardView.classList.remove('flip');
+                    secondCardView.classList.remove('flip');
+                    game.unflipCards();
+                }, 1000)
+            }
         }
     }
+}
 
-    
+function restart(){
+    game.clearCards();
+    startGame();
+    let gameOverLayer =document.getElementById("gameOver");
+    gameOverLayer.style.display = 'none'; 
 }
